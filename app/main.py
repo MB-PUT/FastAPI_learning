99ea4fd2b77ae4aec5036e01d505b1dad27f6c2e
MB-PUT <fe4r.mb@gmail.com>
2023-08-29 11:50:51 +0200

    Daily commit 29/08/2023

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import post, user, auth, vote
 from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
 
-#(11:35:50)
+#(14:17:17)
 
 #models.Base.metadata.create_all(bind=engine)
 
